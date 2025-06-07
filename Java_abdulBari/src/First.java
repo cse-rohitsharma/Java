d780bb6f9598ad428c4db762a34b108ba98ef83d
cse-rohitsharma <cse.rohitsharma7@gmail.com>
2025-06-07 11:33:33 +0530

    Initial commit

---
+++ Java_abdulBari/src/First.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class First{
+    public static void main(String[] args){
+
+        String name;
+
+        Scanner s=new Scanner(System.in);
+        System.out.println("May I know your name?");
+        name = s.nextLine();
+
+        System.out.println("Welcome " + name);
+        
+    }
+}
