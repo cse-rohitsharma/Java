2ed62ab3cca2fd475f7de7be8281d18ed2389b78
cse-rohitsharma <cse.rohitsharma7@gmail.com>
2025-06-26 11:17:24 +0530

    Update Left_Half_Pyramid.java

--- Java_abdulBari/src/JavaPatterns/Left_Half_Pyramid.java
+++ Java_abdulBari/src/JavaPatterns/Left_Half_Pyramid.java
@@ -1,23 +1,18 @@
 package JavaPatterns;
-
 public class Left_Half_Pyramid {
     public static void main (String [] args){
         {   int n=6;
             int i, j;
 
-
             for (i = n; i >= 1; i--) {
-
 
                 for (j = 1; j < i; j++) {
                     System.out.print(" ");
                 }
 
-
                 for (j = 0; j <= n - i; j++) {
                     System.out.print("*");
                 }
-
 
                 System.out.println();
             }
